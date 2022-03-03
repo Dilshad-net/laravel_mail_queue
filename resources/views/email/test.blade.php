658dceb72d1404579b4802241217f6fce9418088
Dilshad-net <dilshadthajudeen@gmail.com>
2022-03-04 02:10:27 +0530

    initial commit

---
+++ resources/views/email/test.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>How to send mail using queue in Laravel 8? - HDTuto.com</title>
+
+</head>
+
+<body>
+
+<center>
+
+<h2 style="padding: 23px;background: #b3deb8a1;border-bottom: 6px green solid;">
+
+    <a href="https://127.0.0.1:8000">Visit Our Website : Myma.com</a>
+
+</h2>
+
+</center>
+
+<p>Hi, Sir</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium neque placeat sint temporibus assumenda. Aliquid odio commodi neque voluptate doloremque, ullam asperiores veniam alias debitis quo, quasi, doloribus officiis ut.</p>
+<strong>Thank you Sir. :)</strong>
+
+  
+
+</body>
+
+</html>
